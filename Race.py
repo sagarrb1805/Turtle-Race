eb5cad323926c2b20d91eefbe134e6cdaebe8485
SagarR.B <sagarrb1805@gmail.com>
2020-12-25 19:01:29 +0530

    added the result in the screen

--- Race.py
+++ Race.py
@@ -4,11 +4,9 @@ import random
 
 
 class Player(turtle.Turtle):
-    def __init__(self):
+    def __init__(self, color, shape):
         turtle.Turtle.__init__(self)
         self.speed_of_turtle = random.uniform(1, 1.5)
-
-    def color_shape(self, color, shape):
         self.color(color)
         self.shape(shape)
 
@@ -22,24 +20,24 @@ class Player(turtle.Turtle):
         self.setheading(self.head)
 
     def race(self):
-
         self.speed(self.speed_of_turtle)
         self.forward(400)
 
 
-t1 = Player()
-t2 = Player()
-t3 = Player()
-t1.color_shape("black", "turtle")
-t2.color_shape("red", "turtle")
-t3.color_shape("green", "turtle")
-t1.set_possition(-200, -200, 90)
+t1 = Player("black", "turtle")
+t2 = Player("red", "turtle")
+t3 = Player("green", "turtle")
+
+
+t1.set_possition(-300, -200, 90)
 t2.set_possition(0, -200, 90)
-t3.set_possition(200, -200, 90)
+t3.set_possition(300, -200, 90)
 
-run = False
+
+
+
+
 def start_race():
-
     thread1 = threading.Thread(target=t1.race)
     thread2 = threading.Thread(target=t2.race)
     thread3 = threading.Thread(target=t3.race)
@@ -52,20 +50,16 @@ def start_race():
 
 speed_list = [t1.speed_of_turtle, t2.speed_of_turtle, t3.speed_of_turtle]
 max_speed = max(speed_list)
-min_speed = min(speed_list)
+
 
 def result():
-    print(t1.speed_of_turtle)
-    print(t2.speed_of_turtle)
-    print(t3.speed_of_turtle)
     for speed in speed_list:
         if max_speed == speed:
             print('the max speed is for', speed_list.index(speed) + 1)
+            turtle.write(arg="The winner is {}".format(speed_list.index(speed) + 1), move=True, align="center", font=("Verdana", 30, "normal"))
+
+
 turtle.listen()
 turtle.onkey(start_race, "Up")
-
-
-
-
 
 turtle.Screen().exitonclick()
