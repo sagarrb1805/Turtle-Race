f282a82ac83a569f39439d0300da8503d6f325b7
SagarR.B <sagarrb1805@gmail.com>
2020-12-25 12:53:11 +0530

    implemented the synchronised movement of turtles with threading

--- Race.py
+++ Race.py
@@ -1,4 +1,6 @@
 import turtle
+import threading
+import random
 
 t1 = turtle.Turtle()
 t2 = turtle.Turtle()
@@ -19,4 +21,28 @@ t3.setpos((200, -200))
 t1.setheading(90)
 t2.setheading(90)
 t3.setheading(90)
+
+
+def t1_forward():
+    t1.speed(random.uniform(1, 1.5))
+    t1.forward(400)
+
+
+def t2_forward():
+    t2.speed(random.uniform(1, 1.5))
+    t2.forward(400)
+
+
+def t3_forward():
+    t3.speed(random.uniform(1, 1.5))
+    t3.forward(400)
+
+
+thread1 = threading.Thread(target=t1_forward)
+thread2 = threading.Thread(target=t2_forward)
+thread3 = threading.Thread(target=t3_forward)
+thread1.start()
+thread2.start()
+thread3.start()
+
 turtle.Screen().exitonclick()
